f2e1f0ab891f90453ab9ef20a2ff74d57dd6ba97
gingaminga <cjhwaan@gmail.com>
2026-01-19 23:37:31 +0900

    [WOWME-41] 파싱 시 10초 타임아웃 설정

--- src/services/blog.service.ts
+++ src/services/blog.service.ts
@@ -29,6 +29,7 @@ export class BlogService implements IBlogService {
         "User-Agent":
           "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
       },
+      timeout: 10_000, // 10초 타임아웃
     });
   }
 
